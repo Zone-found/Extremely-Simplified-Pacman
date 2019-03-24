c536a7a1f3225ba22d273e50c1d9f96a74b1787e
Zone-found <46061640+Zone-found@users.noreply.github.com>
2019-03-24 20:40:00 +0800

    Add files via upload

---
+++ event.h
@@ -0,0 +1,23 @@
+//
+//  event.h
+//  Spanner
+//
+//  Created by Saturn&Eric on 2018/12/30.
+//  Copyright © 2018年 Bakantu. All rights reserved.
+//
+//  胡大佬深夜拔刀相助，小生没齿难忘 /斜眼笑
+
+#ifndef event_hpp
+#define event_hpp
+
+/*********************************
+ *
+ *事件注册函数声明头文件
+ *
+ ***/
+
+void start_onClick(void);
+void about_onClick(void);
+void exit_onClick(void);
+
+#endif /* event_hpp */
---
+++ gamestart.h
@@ -0,0 +1,17 @@
+#ifndef _GAMESTART_H_
+#define _GAMESTART_H_
+#include "../include/main.h"
+#include "../include/view.h"
+#include "../include/event.h"
+#include <iterator>
+
+void welcomeScreen();
+void welcomeMenu();
+void display();
+void reshape(int w, int h);
+
+
+
+
+
+#endif
