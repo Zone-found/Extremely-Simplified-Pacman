0c01235ae2aa878664f433544eaa4cb10bacb9a7
Zone-found <46061640+Zone-found@users.noreply.github.com>
2019-03-24 20:45:32 +0800

    Add files via upload

---
+++ include/createpacman.h
@@ -0,0 +1,11 @@
+#ifndef _CREATEPACMAN_H_
+#define _CREATEPACMAN_H_
+
+#ifdef __APPLE__
+#include <OpenGL/OpenGL.h>
+#include <GLUT/glut.h>
+#endif
+
+void drawPacman(float positionX, float positionY, float rotation);
+
+#endif
